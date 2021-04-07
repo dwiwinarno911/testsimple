8dc72f4fff29fe62abfdd4befd3075ea4dff2f84
dwiwinarno911 <dwibluefeathers@gmail.com>
2021-04-07 14:07:07 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Test From</title>
+</head>
+<body>
+	<br>
+	<br>
+	<br>
+<table width="20%" align="center" border="1">
+	<tr>
+		<td>Nama</td>
+		<td>: Siska</td>
+	</tr>
+	<tr>
+		<td>No Hp</td>
+		<td>: 09787986798987</td>
+	</tr>
+	<tr>
+		<td colspan="2" align="center">
+			<button onclick="location.href = 'pegawai.php';">Pegawai</button>
+		</td>
+	</tr>
+	<tr>
+		<td colspan="2" align="center">
+			<button onclick="location.href = 'pelanggan.php';">Pelanggan</button>
+		</td>
+	</tr>
+	<tr>
+		<td colspan="2" align="center">
+			<button onclick="location.href = 'barang.php';">Barang</button>
+		</td>
+	</tr>
+</table>
+</body>
+</html>
